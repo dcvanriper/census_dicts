73142a49d8dbadd0346249008995d92c6550c077
dcvanriper <dvanriper@gmail.com>
2017-12-15 07:57:09 -0600

    Create state_abbreviations_lower.py

---
+++ state_abbreviations_lower.py
@@ -0,0 +1,2 @@
+#Python array of state abbreviations - lower case
+states_lower = ['al', 'ak', 'az', 'ar', 'ca', 'co', 'ct', 'dc', 'de', 'fl', 'ga', 'hi', 'id', 'il', 'in', 'ia', 'ks', 'ky', 'la', 'me', 'md', 'ma', 'mi', 'mn', 'ms', 'mo', 'mt', 'ne', 'nv', 'nh', 'nj', 'nm', 'ny', 'nc', 'nd', 'oh', 'ok', 'or', 'pa', 'ri', 'sc', 'sd', 'tn', 'tx', 'ut', 'vt', 'va', 'wa', 'wv', 'wi', 'wy']
